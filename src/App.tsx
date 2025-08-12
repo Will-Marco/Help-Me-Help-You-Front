fb7dd01586529b86483ccdf01a78fc646c9985b2
Azamat <azamatomonkeldiyev@gmail.com>
2025-08-12 15:36:46 +0500

    added reting page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { useCookies } from "react-cookie";
 import { Toaster } from "sonner";
 import "./App.css";
 import AuthRoute from "./routes/auth/AuthRoute";
-// import DashboardLayout from "./features";
-import { StudentHome } from "./pages/Student";
+import DashboardLayout from "./features";
+// import { StudentHome } from "./pages/Student";
 
 function App() {
   const [cookies] = useCookies(["token"]);
 
   return (
     <>
-      {cookies.token ? <StudentHome /> : <AuthRoute />}
-      {/* {cookies.token ? <DashboardLayout /> : <AuthRoute />} */}
+      {/* {cookies.token ? <StudentHome /> : <AuthRoute />} */}
+      {cookies.token ? <DashboardLayout /> : <AuthRoute />}
 
       <Toaster position="top-right" richColors />
     </>
